359deebb87a7c791740a4ccfbf1937413c0629eb
ConstasJ <constasj@qq.com>
2026-04-21 17:28:49 +0800

    chore: 添加基础npm依赖项

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,24 +1,6 @@
 import { defineConfig } from 'tsdown'
-import obfuscator from 'rollup-plugin-obfuscator'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  outDir: './dist',
-  dts: true,
   minify: true,
-  plugins: [
-    obfuscator({
-        global: false,
-        options: {
-            compact: true,
-            controlFlowFlattening: true,
-            deadCodeInjection: true,
-            debugProtection: true,
-            debugProtectionInterval: 4000,
-            disableConsoleOutput: true,
-            sourceMap: true,
-        },
-        exclude: ['node_modules/**', '**/*.d.ts']
-    })
-  ]
 })
